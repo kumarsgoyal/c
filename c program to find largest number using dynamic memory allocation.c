570d482cc59f661cc984a140ca76f4f6dc33407f
kumarsgoyal <43093347+kumarsgoyal@users.noreply.github.com>
2018-09-25 17:55:28 +0530

    Create c program to find largest number using dynamic memory allocation.c

---
+++ c program to find largest number using dynamic memory allocation.c
@@ -0,0 +1,46 @@
+// . Depending upon the number of elements, the required size is allocated which prevents the wastage of memory.
+// . If no memory is allocated, error is displayed and the program is terminated.
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, num;
+    float *data;
+
+    printf("Enter total number of elements(1 to 100): ");
+    scanf("%d", &num);
+
+    // Allocates the memory for 'num' elements.
+    data = (float*) calloc(num, sizeof(float));
+
+    if(data == NULL)
+    {
+        printf("Error!!! memory not allocated.");
+        exit(0);
+    }
+
+    printf("\n");
+
+    // Stores the number entered by the user.
+    for(i = 0; i < num; ++i)
+    {
+       printf("Enter Number %d: ", i + 1);
+       scanf("%f", data + i);
+    }
+
+    // Loop to store largest number at address data
+    for(i = 1; i < num; ++i)
+    {
+       // Change < to > if you want to find the smallest number
+       if(*data < *(data + i))
+           *data = *(data + i);
+    }
+
+    printf("Largest element = %.2f", *data);
+
+    return 0;
+}
